a25f89beec1766d6399df9c77880e6d6f9d90572
Carlos Herrera <juan.cervantes@rappi.com>
2022-04-24 10:20:09 -0500

    EB-78 Changed paths for avatar and photos resources

--- plugins/auth.js
+++ plugins/auth.js
@@ -6,8 +6,7 @@ module.exports = {
     return (req, res, next) => {
       const userType = actionParams.valueKey == 'header' ?
         req.headers['user-type'] : req.body.user.type;
-      console.log('ROLES: ', actionParams.roles);
-      console.log('USER TYPE: ', userType);
+
       if (actionParams.roles.includes(userType)) {
         return next();
       }
