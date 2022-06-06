adc06e5107d09891b752007f246add5186015edf
Carlos Herrera <43161662+carlos-herrera-cervantes@users.noreply.github.com>
2022-06-06 01:18:10 -0500

    EB-119 Fixed login when Kafka cluster is down

--- services/producer.js
+++ services/producer.js
@@ -7,6 +7,9 @@ class Producer {
     const kafka = new Kafka({
       clientId: process.env.KAFKA_CLIENT_ID,
       brokers: [process.env.KAFKA_BROKERS],
+      retry: {
+        retries: 0,
+      },
     });
 
     this.producer = kafka.producer();
